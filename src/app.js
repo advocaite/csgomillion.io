f95d2b80acedcee8bdfb41de37a1f24f6be7110a
Leonardo Moreira <eu@leonardomoreira.com.br>
2015-08-19 15:48:16 -0300

    Chat brodcast return

--- src/app.js
+++ src/app.js
@@ -3,31 +3,33 @@ var http = require('http').Server(app);
 var io   = require('socket.io')(http);
 var fs   = require('fs');
 
+var online = 0;
+var users = {};
+
 app.get('/', function(req, res){
     res.send('<pre>csgomillion.com</pre>');
 });
 
 io.on('connection', function(socket){
 
+    // Increase users online.
+    online++;
+    socket.emit('chat:online', online);
+
     console.log('Guet user connected');
 
     socket.on('disconnect', function() {
+        // Decrease users online.
+        online--;
+        socket.emit('chat:online', online);
+
         console.log('Guest user disconnected');
     });
-
-    //Broadcast a message to connected users when someone connects or disconnects
-    //Add support for nicknames
-    //    Don’t send the same message to the user that sent it himself. Instead, append the message directly as soon as he presses enter.
-    //    Add “{user} is typing” functionality
-    //Show who’s online
-    //Add private messaging
-    //Share your improvements!
 
     socket.on('chat:message', function(data) {
         socket.broadcast.emit('chat:message', data);
         console.log(data.user.PERSON_NAME + ' type on chat : ' + data.message.text);
     });
-
 });
 
 http.listen(3000, function(){
