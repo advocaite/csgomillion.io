ed6c639a43d896e04bd03923b2997def7e9d71b1
Leonardo Moreira <eu@leonardomoreira.com.br>
2015-08-19 15:18:00 -0300

    Chat brodcast return

--- src/app.js
+++ src/app.js
@@ -15,19 +15,17 @@ io.on('connection', function(socket){
         console.log('Guest user disconnected');
     });
 
+    //Broadcast a message to connected users when someone connects or disconnects
+    //Add support for nicknames
+    //    Don’t send the same message to the user that sent it himself. Instead, append the message directly as soon as he presses enter.
+    //    Add “{user} is typing” functionality
+    //Show who’s online
+    //Add private messaging
+    //Share your improvements!
+
     socket.on('chat:message', function(data) {
-
         socket.broadcast.emit('chat:message', data);
-
-        //Broadcast a message to connected users when someone connects or disconnects
-        //Add support for nicknames
-        //    Don’t send the same message to the user that sent it himself. Instead, append the message directly as soon as he presses enter.
-        //    Add “{user} is typing” functionality
-        //Show who’s online
-        //Add private messaging
-        //Share your improvements!
-
-        console.log(data.user.PERSON_NAME + ' type on chat : ' + data.message);
+        console.log(data.user.PERSON_NAME + ' type on chat : ' + data.message.text);
     });
 
 });
