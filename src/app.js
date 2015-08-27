36baad74497a1893e7eb1388f0c213ce5724eb2c
Leonardo Moreira <eu@leonardomoreira.com.br>
2015-08-27 17:52:23 -0300

    Config Heroe

--- src/app.js
+++ src/app.js
@@ -113,16 +113,18 @@ var jackpot = {
             dataType: 'json'
         }).then(function(response) {
 
-            var data = response.getBody();
+            console.log(response.getBody());
 
-            roulette = {
-                ease    : data.game.EASE,
-                million : data.game.MILLION,
-                winner  : data.game.WINNER,
-                items   : data.game.ROULETTE
-            };
-
-            jackpot.winner(roulette);
+            //var data = response.getBody();
+            //
+            //roulette = {
+            //    ease    : data.game.EASE,
+            //    million : data.game.MILLION,
+            //    winner  : data.game.WINNER,
+            //    items   : data.game.ROULETTE
+            //};
+            //
+            //jackpot.winner(roulette);
         });
 
     },
